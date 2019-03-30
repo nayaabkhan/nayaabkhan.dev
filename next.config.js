7a37be5bf2661a08a6e42c55fa3ac9bfc0e63cc7
Nayaabkhan Khan <khannayaab@gmail.com>
2019-03-30 16:01:42 +0530

    Add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,26 +1,42 @@
 const withOptimizedImages = require('next-optimized-images')
 const withMDX = require('@zeit/next-mdx')()
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
 
 module.exports = withMDX(
-  withOptimizedImages({
-    target: 'serverless',
-    pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+  withOptimizedImages(
+    withBundleAnalyzer({
+      analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+      analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+      bundleAnalyzerConfig: {
+        server: {
+          analyzerMode: 'static',
+          reportFilename: '../bundles/server.html',
+        },
+        browser: {
+          analyzerMode: 'static',
+          reportFilename: '../bundles/client.html',
+        },
+      },
 
-    webpack: (config, { defaultLoaders }) => {
-      config.module.rules.push({
-        test: /\.css$/,
-        use: [
-          defaultLoaders.babel,
-          {
-            loader: require('styled-jsx/webpack').loader,
-            options: {
-              type: 'global',
+      target: 'serverless',
+      pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+
+      webpack: (config, { defaultLoaders }) => {
+        config.module.rules.push({
+          test: /\.css$/,
+          use: [
+            defaultLoaders.babel,
+            {
+              loader: require('styled-jsx/webpack').loader,
+              options: {
+                type: 'global',
+              },
             },
-          },
-        ],
-      })
+          ],
+        })
 
-      return config
-    },
-  })
+        return config
+      },
+    })
+  )
 )
